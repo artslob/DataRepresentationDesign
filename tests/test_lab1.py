3313e4b25791ed474a26df04d4bf8219371bca69
artslob <artslob@yandex.ru>
2019-02-21 11:54:57 +0300

    tests for insert operation

--- tests/test_lab1.py
+++ tests/test_lab1.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import pytest
 
-from lab1.exceptions import TableCreationError
+from lab1.exceptions import TableCreationError, TableInsertError
 from lab1.tables import Table, Column
-from lab1.types import IntType, StringType, FloatType
+from lab1.types import IntType, StringType, FloatType, DatetimeType, BinaryType
 
 
 def test_table_creation():
@@ -46,3 +48,79 @@ def test_create_table_with_wrong_column_type(wrong_value, expected_exc):
 def test_create_table_duplicate_fields():
     with pytest.raises(TableCreationError, match='already exist'):
         Table(columns=[Column('id', IntType), Column('name', StringType), Column('id', FloatType)])
+
+
+def test_insert():
+    table = Table(columns=(Column('id', IntType), Column('name', StringType), Column('weight', FloatType),))
+    assert len(table.rows) == 0
+    first_arg = dict(id=1, name='John', weight=70.)
+    second_arg = dict(id=2, name='Steven', weight=75.)
+    table.insert(first_arg)
+    table.insert(second_arg)
+    assert len(table.rows) == 2
+    assert table.rows[0] == first_arg
+    assert table.rows[1] == second_arg
+
+
+def test_insert_mutability():
+    table = Table(columns=(Column('id', IntType),))
+    value = dict(id=3)
+    table.insert(value)
+    assert table.rows[0] == value
+    value['id'] = 5
+    assert table.rows[0]['id'] == 5
+
+
+@pytest.mark.parametrize('value', [dict, str, list, 12, 'string', tuple(), []])
+def test_insert_not_dict(value):
+    table = Table(columns=(Column('id', IntType),))
+    with pytest.raises(TableInsertError, match='should be dictionary'):
+        table.insert(value)
+
+
+@pytest.mark.parametrize('value', [{}, dict(id=2), dict(id=2, name=3, weight=7.)])
+def test_insert_wrong_number_of_args(value):
+    table = Table(columns=(Column('id', IntType), Column('name', StringType),))
+    with pytest.raises(TableInsertError, match='number .* not equal to'):
+        table.insert(value)
+
+
+def test_insert_missing_field_name():
+    table = Table(columns=(Column('id', IntType), Column('name', StringType),))
+    with pytest.raises(TableInsertError, match='missing field name'):
+        table.insert(dict(id=1, test='str'))
+
+
+@pytest.mark.parametrize('value', [7.0, datetime(2019, 1, 2), 'str', b'bytes'])
+def test_insert_wrong_type_int(value):
+    table = Table(columns=(Column('id', IntType),))
+    with pytest.raises(TableInsertError, match='invalid value for field'):
+        table.insert(dict(id=value))
+
+
+@pytest.mark.parametrize('value', [42, datetime(2019, 1, 2), 'str', b'bytes'])
+def test_insert_wrong_type_float(value):
+    table = Table(columns=(Column('temperature', FloatType),))
+    with pytest.raises(TableInsertError, match='invalid value for field'):
+        table.insert(dict(temperature=value))
+
+
+@pytest.mark.parametrize('value', [42, 42.0, 'str', b'bytes'])
+def test_insert_wrong_type_datetime(value):
+    table = Table(columns=(Column('birthday', DatetimeType),))
+    with pytest.raises(TableInsertError, match='invalid value for field'):
+        table.insert(dict(birthday=value))
+
+
+@pytest.mark.parametrize('value', [42, 7.0, datetime(2019, 1, 2), b'bytes'])
+def test_insert_wrong_type_str(value):
+    table = Table(columns=(Column('name', StringType),))
+    with pytest.raises(TableInsertError, match='invalid value for field'):
+        table.insert(dict(name=value))
+
+
+@pytest.mark.parametrize('value', [42, 7.0, datetime(2019, 1, 2), 'str'])
+def test_insert_wrong_type_binary(value):
+    table = Table(columns=(Column('file', BinaryType),))
+    with pytest.raises(TableInsertError, match='invalid value for field'):
+        table.insert(dict(file=value))
