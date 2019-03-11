5fe5196e8467a01ea69b8a92e4e2317f600b26fc
artslob <artslob@yandex.ru>
2019-03-11 18:00:09 +0300

    move function creation to fixture

--- tests/lab1/test_update.py
+++ tests/lab1/test_update.py
@@ -1,15 +1,29 @@
+import pytest
+
 from lab1.tables import Table, Column
 from lab1.types import IntType, StringType, FloatType
 
 
-def test_update():
-    table = Table(columns=(Column('id', IntType), Column('name', StringType), Column('age', FloatType),))
-    table.insert(dict(id=1, name='George', age=35.5))
-    table.insert(dict(id=2, name='Fred', age=25.))
+@pytest.fixture
+def table_factory():
+    def create_table():
+        table = Table(columns=(Column('id', IntType), Column('name', StringType), Column('age', FloatType),))
+        table.insert(dict(id=1, name='George', age=35.5))
+        table.insert(dict(id=2, name='Fred', age=25.))
+        return table
+
+    return create_table
+
+
+@pytest.fixture
+def table(table_factory):
+    return table_factory()
+
+
+def test_update(table):
     assert len(table.rows) == 2
     table.update(dict(name='Test'))
     assert table.rows == [dict(id=1, name='Test', age=35.5), dict(id=2, name='Test', age=25.)]
     assert table.select(['name']) == [['Test'], ['Test']]
     table.update(dict(id=1000), table.fields['id'] == 1)
     assert table.select(['id']) == [[1000], [2]]
-
