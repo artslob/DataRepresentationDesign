50b7c0f7f358175ecb6c3fa3232cfeed11563563
artslob <artslob@yandex.ru>
2019-03-11 17:56:18 +0300

    add test of deletion with condition

--- tests/lab1/test_delete.py
+++ tests/lab1/test_delete.py
@@ -33,3 +33,6 @@ def test_delete_condition(table):
     table.delete(table.fields['name'] == 'Fred')
     assert len(table.rows) == 1
     assert table.rows == [dict(id=1, name='George', age=35.5)]
+    table.delete(table.fields['age'] <= 40.)
+    assert len(table.rows) == 0
+    assert table.rows == []
