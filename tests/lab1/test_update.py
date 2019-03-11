30cac9455defb68a46d1afc4cdf757c663e4077a
artslob <artslob@yandex.ru>
2019-03-11 18:27:44 +0300

    add update test with conditions

--- tests/lab1/test_update.py
+++ tests/lab1/test_update.py
@@ -27,3 +27,12 @@ def test_update(table):
     assert table.select(['name']) == [['Test'], ['Test']]
     table.update(dict(id=1000), table.fields['id'] == 1)
     assert table.select(['id']) == [[1000], [2]]
+
+
+def test_update_conditions(table):
+    table.update(dict(name='Test'), table.fields['id'] < 0)
+    assert table.select(['name']) == [['George'], ['Fred']]
+    table.update(dict(name='Test'), (1 < table.fields['id']) & (table.fields['age'] > 20.))
+    assert table.select(['name']) == [['George'], ['Test']]
+    table.update(dict(name='2'), (1 < table.fields['id']) | (30. < table.fields['age']))
+    assert table.select(['name']) == [['2'], ['2']]
